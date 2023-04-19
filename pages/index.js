d45df97a2c4b0077f75d314aa393e4bb6b12b9a6
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2023-04-19 19:39:53 +0530

    main code

--- pages/index.js
+++ pages/index.js
@@ -1,123 +1,241 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import { Plus, Trash2 } from "react-feather";
+import { useState } from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
-export default function Home() {
+const Item = ({ idx, item, removeItem, handleEdit }) => {
+  const [showEditOptions, setShowEditOptions] = useState(false);
+
+  const [currItem, setCurrItem] = useState(item);
+  const [isRequired, setIsRequired] = useState(currItem?.isRequired);
+
+  const [list, setList] = useState({ children: [] });
+
+  const clickHandler = (item) => {
+    const newItem = {
+      id: uuidv4(),
+      name: "name",
+      type: "string",
+      isRequired: false,
+    };
+
+    setList({ children: [...list.children, newItem] });
+
+    handleEdit({ ...item, children: [...list.children, newItem] });
+
+    // update the main list
+    // new item should have a children key
+    // and the value shoule be our current list
+  };
+
+  const removeChildItem = (idx) => {
+    const filterdItems = list.children.filter((_, index) => index !== idx);
+
+    setList({ children: [...filterdItems] });
+  };
+
+  const handleChildEdit = (currItem) => {
+    const childIndex = list.children.findIndex(
+      (child) => child.id === currItem.id
+    );
+
+    const newChildren = [...list.children];
+    newChildren[childIndex] = {
+      ...newChildren[childIndex],
+      isRequired: isRequired,
+    };
+
+    setList({ children: newChildren });
+  };
+
+  // there should be a required for child also
+
+  // whenever we child on add button of a item
+  // we should add a new key called childern : and create a new list and add items into it
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Namaskar Mandali!
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className=" w-full">
+      <div
+        onMouseEnter={() => setShowEditOptions(true)}
+        onMouseLeave={() => setShowEditOptions(false)}
+        className={`flex items-center justify-between  w-full `}
+      >
+        <div className=" hover:bg-gray-200 py-1 border-b-2    flex items-center justify-between w-11/12">
+          <div className="flex items-center ">
+            <input
+              value={currItem?.name}
+              onChange={(e) => {
+                const newItem = { ...currItem, name: e.target.value };
+                setCurrItem(newItem);
+                handleEdit(newItem);
+              }}
+              className="font-semibold focus:w-32 w-28  outline-none  bg-inherit focus:bg-white rounded px-1"
             />
-          </a>
+            <select
+              id="type-select"
+              className="font-semibold uppercase bg-gray-200 rounded"
+              value={currItem?.type}
+              onChange={(e) => {
+                const newItem = { ...currItem, type: e.target.value };
+                setCurrItem(newItem);
+                handleEdit(newItem);
+              }}
+            >
+              <option value="string">String</option>
+              <option value="integer">Integer</option>
+              <option value="boolean">Boolean</option>
+              <option value="object">Object</option>
+              <option value="array">Array</option>
+            </select>
+          </div>
+          {showEditOptions && (
+            <div className="flex items-center gap-4 ">
+              <label className="flex items-center gap-2 cursor-pointer">
+                <div className="ml-3  font-medium">Required</div>
+                <div className="relative">
+                  <input
+                    type="checkbox"
+                    className="sr-only"
+                    checked={isRequired}
+                    onChange={(e) => {
+                      const newItem = {
+                        ...currItem,
+                        isRequired: e.target.checked,
+                      };
+                      setIsRequired(e.target.checked);
+                      setCurrItem(newItem);
+                      handleEdit(newItem);
+                    }}
+                  />
+                  <div
+                    className={`block  w-12 h-6 rounded-full ${
+                      isRequired ? "bg-blue-500" : " bg-gray-400"
+                    }`}
+                  ></div>
+                  <div
+                    className={`absolute top-1 left-1 bg-white w-4 h-4 rounded-full transition ${
+                      isRequired ? "translate-x-6" : ""
+                    }`}
+                  ></div>
+                </div>
+              </label>
+              {currItem.type === "object" && (
+                <button
+                  onClick={() => clickHandler(item)}
+                  className="bg-white p-1  rounded-lg"
+                >
+                  <Plus className="bg-white  rounded-lg w-4 h-4" />
+                </button>
+              )}
+              <button onClick={() => removeItem(idx)}>
+                <Trash2 className=" w-5 h-5 " />
+              </button>
+            </div>
+          )}
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {list?.children.length > 0 ? (
+        <div className=" p-4 ">
+          {list.children.map((item, idx) => {
+            return (
+              <div key={idx}>
+                <div
+                  className={` flex   
+       
+               `}
+                >
+                  <div
+                    className={`  ${
+                      list?.children?.length > 1 && "border-l"
+                    }  mr-4 w-4 ${
+                      list?.children?.length > 1 && idx === 0 && "border-t "
+                    }
+                ${
+                  list?.children?.length > 1 &&
+                  idx === list.children.length - 1 &&
+                  "border-b"
+                } `}
+                  ></div>
+                  <Item
+                    idx={idx}
+                    item={item}
+                    removeItem={removeChildItem}
+                    handleEdit={handleChildEdit}
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default function Home() {
+  const [list, setList] = useState({
+    children: [],
+  });
+
+  const clickHandler = () => {
+    const newItem = {
+      name: "name",
+      type: "string",
+      isRequired: false,
+    };
+
+    setList({ children: [...list.children, newItem] });
+  };
+
+  const removeItem = (idx) => {
+    const filterdItems = list.children.filter((_, index) => index !== idx);
+
+    setList({ children: [...filterdItems] });
+  };
+
+  const handleEdit = (currItem) => {
+    const itemIndex = list.children.findIndex(
+      (habit, _) => habit.id === currItem.id
+    );
+
+    list.children[itemIndex] = currItem;
+    setList({ children: [...list.children] });
+  };
+
+  return (
+    <div className={`flex h-screen bg-[#F5F5F5] ${plusJakartaSans}`}>
+      <div className=" bg-white border-2 border-gray-300 shadow-md p-8 w-full max-w-2xl m-auto rounded-md ">
+        <div className="bg-gray-50 p-4 space-y-4">
+          <div className="flex justify-between items-center">
+            <p className="font-bold text-gray-600 ">Feild name and type</p>
+            <button
+              onClick={() => clickHandler()}
+              className="bg-white p-1 rounded-lg"
+            >
+              <Plus className="bg-white  rounded-lg w-5 h-5" />
+            </button>
+          </div>
+          <main>
+            {list.children.map((item, idx) => {
+              return (
+                <div key={idx} className="flex    ">
+                  <p className="text-gray-400 py-2 ">{idx + 1}.</p>
+
+                  <Item
+                    idx={idx}
+                    item={item}
+                    removeItem={removeItem}
+                    handleEdit={handleEdit}
+                  />
+                </div>
+              );
+            })}
+          </main>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
